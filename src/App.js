6c4d0754ea6dcb1babcfe560fdf0346c68ae9eb0
Rogers Wase <waserogers@gmail.com>
2025-05-21 00:35:10 +0100

    Exercising some basic pronciples to build a card app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+import './index.css';
+
+function App() {
+    return <div className="app">
+        <a href="tel:+639988064140">Call Me</a>
+    </div>
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from './App';
 import "./index.css";
-import WhatIsYourName from "./YourName";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div className="card-box">
-      <WhatIsYourName uname="Rogers" date="12/03/1984" />
-      <WhatIsYourName uname="Clinton" date="01/05/1990" />
-      <WhatIsYourName uname="Adephus" date="09/09/1995" />
-      <WhatIsYourName uname="Junior" date="17/06/1991" />
-    </div>
+    <App/>
+   
   </React.StrictMode>
 );
